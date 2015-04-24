b55e5fa07e2c9cef468956cf7daab9800c8ba871
Eni <Mustafaraj>
2015-04-23 20:54:14 -0400

    Add functions to display D3 charts
    SVG elements are wrapped in templates. Notice Tracker.autorun to update
    the second SVG whenever a click of the first one happens.

--- meteor_d3_example.js
+++ meteor_d3_example.js
@@ -1,6 +1,43 @@
 if (Meteor.isClient){
   
-  Session.set("name", "D3");
+  Session.set("points", []);
   console.log("I'm running client code");
   
+  function draw(svg, point){
+     svg.append("circle")
+        .attr("cx", point.x)
+        .attr("cy", point.y)
+        .attr("r", 2);
+  }
+  
+  Template.clickSVG.onRendered(function(){
+    // Once the svg is added to the DOM, register the event handler method
+    // for the "click" event.
+      var svgClick = d3.select("#clickArea");
+      svgClick.on("click", getPosition);
+
+      function getPosition(){
+        var position = d3.mouse(this);
+        var point = {"x": position[0], "y": position[1]};
+        points = Session.get("points");
+        points.push(point);
+        Session.set("points", points);
+        draw(svgClick, point)
+      }
+    
+  });
+  
+  /* This function is a reactive context that will run whenever it
+  senses changes in the Session (which is a reactive data source).
+  */
+  Tracker.autorun(function(){
+    var length = Session.get("points").length;
+    var svgChart = d3.select("#chartArea");
+    if (length > 0) {
+      var point = Session.get("points")[length-1];
+      draw(svgChart, {"x": length*5, "y": point.y});
+    }
+    
+  })
+    
 }
